8d947ccfcbf172e0aee5644d2c359efa60ff1013
Kate Ramsey <kateramsey@live.com>
2016-04-19 16:28:19 -0500

    added seed method

--- Authentication Practice/Migrations/Configuration.cs
+++ Authentication Practice/Migrations/Configuration.cs
@@ -1,3 +1,7 @@
+using Authentication_Practice.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
 namespace Authentication_Practice.Migrations
 {
     using System;
@@ -16,16 +20,59 @@ namespace Authentication_Practice.Migrations
         {
             //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            if (!context.Roles.Any())
+            {
+                roleManager.Create(new IdentityRole("Admin"));
+                roleManager.Create(new IdentityRole("Developer"));
+                roleManager.Create(new IdentityRole("NormalUser"));
+            }
+            if (!context.Users.Any(x => x.UserName == "kateramsey@live.com"))
+            {
+                var user = new ApplicationUser
+                {
+                    FirstName = "Kate",
+                    LastName = "Ramsey",
+                    DateOfBirth = DateTime.Parse("5/29/1985"),
+                    Email = "kateramsey@live.com",
+                    UserName = "kateramsey@live.com",
+                    HomeTown = "Little Rock",
+                };
+                userManager.Create(user, "MyP@ssw0rd!");
+          
+                
+
+                var user2 = new ApplicationUser
+                {
+                    FirstName = "Jennifer",
+                    LastName = "Tate",
+                    DateOfBirth = DateTime.Parse("9/19/1984"),
+                    Email = "jstate@live.com",
+                    UserName = "jstate@live.com",
+                    HomeTown = "Dallas",
+                };
+                userManager.Create(user2, "MyP@ssw0rd!");
+
+                var user3 = new ApplicationUser
+                {
+                    FirstName = "Bruce",
+                    LastName = "Wills",
+                    DateOfBirth = DateTime.Parse("1/24/1986"),
+                    Email = "wills_martin@yahoo.com",
+                    UserName = "wills_martin@yahoo.com",
+                    HomeTown = "Little Rock",
+                };
+                userManager.Create(user3, "MyP@ssw0rd!");
+                context.SaveChanges();
+
+                userManager.AddToRole(user3.Id, "NormalUser");
+                userManager.AddToRole(user2.Id, "Developer");
+                userManager.AddToRole(user.Id, "Admin");
+            }
         }
     }
 }
